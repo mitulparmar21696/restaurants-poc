024e90a372f34c959291b52abc1dc4bb4a760e56
Mitul Parmar <mitul@sysbioz.com>
2023-02-27 14:44:47 +0530

    cart handle and price handle done

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,15 +1,37 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import './Layout.scss';
 import { Layout, Badge, Menu, Dropdown } from 'antd';
 import { ShoppingCartOutlined } from '@ant-design/icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { CartItemType } from 'types/cart';
+import { setCartDetails } from 'store/Actions/cart';
+import { RootState } from '../../store';
+
 const { Header, Content } = Layout;
 type LayoutTpe = {
   children: ReactNode;
 };
 
 const MainLayout = ({ children }: LayoutTpe) => {
-  // console.log('props', props);
+  const [orderCount, setOrderCount] = useState<number>(0);
+  const dispatch = useDispatch();
+  const cart = useSelector((state: RootState) => state?.cartReducer);
+  useEffect(() => {
+    const cart: string | null = localStorage.getItem('cart');
+    if (cart) {
+      const cartItems: CartItemType[] = JSON.parse(cart);
 
+      dispatch(setCartDetails(cartItems));
+    }
+  }, []);
+
+  useEffect(() => {
+    let quantity = 0;
+    cart.cartDetails.forEach((e) => {
+      quantity = quantity + e.quantity;
+    });
+    setOrderCount(quantity);
+  }, [cart.cartDetails]);
   const cartMenu = (
     <Menu>
       <Menu.Item key="0">
@@ -27,7 +49,7 @@ const MainLayout = ({ children }: LayoutTpe) => {
           <div className="logo">Pizzaria</div>
           <div className="cart">
             <Dropdown overlay={cartMenu} trigger={['click']}>
-              <Badge count={3} size="small">
+              <Badge count={orderCount} size="small">
                 <ShoppingCartOutlined style={{ fontSize: '24px' }} />
               </Badge>
             </Dropdown>
--- src/pages/PizzaList/PizzaList.tsx
+++ src/pages/PizzaList/PizzaList.tsx
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './PizzaList.scss';
 import { List, Card } from 'antd';
+import { RootState } from '../../store';
 import { CartItemType } from 'types/cart';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCartDetails } from 'store/Actions/cart';
+import { AppButton } from 'components/Base/AppButton';
 
 interface PizzaType {
   id: number;
@@ -14,7 +18,12 @@ interface PizzaType {
 const PizzaList = () => {
   const [selectedPizza, setSelectedPizza] = useState<PizzaType>();
   const [cartItems, setCartItems] = useState<CartItemType[]>([]);
+  const [price, setPrice] = useState<number>(0);
+  const [orderCount, setOrderCount] = useState<number>(0);
 
+  const cart = useSelector((state: RootState) => state?.cartReducer);
+
+  const dispatch = useDispatch();
   const pizzas = [
     {
       id: 1,
@@ -39,29 +48,55 @@ const PizzaList = () => {
     }
   ];
 
+  useEffect(() => {
+    const cart: string | null = localStorage.getItem('cart');
+    if (cart) {
+      const cartItems: CartItemType[] = JSON.parse(cart);
+      setCartItems(cartItems);
+    }
+  }, []);
+
+  useEffect(() => {
+    let quantity = 0;
+    cart.cartDetails.forEach((e: CartItemType) => {
+      quantity = quantity + e.quantity;
+    });
+    setOrderCount(quantity);
+    getPrice();
+  }, [cart.cartDetails]);
+
   const handleAddToCart = (item: PizzaType) => {
     const existingItem = cartItems.find((cartItem) => cartItem.id === item.id);
     if (existingItem) {
-      setCartItems(
-        cartItems.map((cartItem) =>
-          cartItem.id === item.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
-        )
+      const cart = cartItems.map((cartItem) =>
+        cartItem.id === item.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
       );
+      setCartItems(cart);
+      dispatch(setCartDetails(cart));
+      localStorage.setItem('cart', JSON.stringify(cart));
     } else {
-      setCartItems([...cartItems, { ...item, quantity: 1 }]);
+      const cart = [...cartItems, { ...item, quantity: 1 }];
+      setCartItems(cart);
+      dispatch(setCartDetails(cart));
+      localStorage.setItem('cart', JSON.stringify(cart));
     }
   };
 
   const handleRemoveFromCart = (item: PizzaType) => {
     const existingItem = cartItems.find((cartItem) => cartItem.id === item.id);
     if (existingItem && existingItem.quantity > 1) {
-      setCartItems(
-        cartItems.map((cartItem) =>
-          cartItem.id === item.id ? { ...cartItem, quantity: cartItem.quantity - 1 } : cartItem
-        )
+      const cart = cartItems.map((cartItem) =>
+        cartItem.id === item.id ? { ...cartItem, quantity: cartItem.quantity - 1 } : cartItem
       );
+      setCartItems(cart);
+      dispatch(setCartDetails(cart));
+
+      localStorage.setItem('cart', JSON.stringify(cart));
     } else {
-      setCartItems(cartItems.filter((cartItem) => cartItem.id !== item.id));
+      const cart = cartItems.filter((cartItem) => cartItem.id !== item.id);
+      setCartItems(cart);
+      dispatch(setCartDetails(cart));
+      localStorage.setItem('cart', JSON.stringify(cart));
     }
   };
 
@@ -69,90 +104,106 @@ const PizzaList = () => {
     setSelectedPizza(pizza);
   };
 
-  const totalPrice = cartItems.reduce(
-    (total, cartItem) => total + cartItem.price * cartItem.quantity,
-    0
-  );
+  const getPrice = () => {
+    const totalPrice = cartItems.reduce(
+      (total, cartItem) => total + cartItem.price * cartItem.quantity,
+      0
+    );
+    setPrice(totalPrice);
+  };
 
   return (
-    <div className="pizzaContainer">
-      <div style={{ flex: 1, margin: '10px' }}>
-        <List
-          dataSource={pizzas}
-          renderItem={(pizza: PizzaType) => (
-            <List.Item>
-              <div>
-                <img
-                  width={50}
-                  height={50}
-                  src={pizza.url}
-                  onClick={() => handleSelectPizza(pizza)}
-                />
-              </div>
-              <div className="pizzaDetails" onClick={() => handleSelectPizza(pizza)}>
-                <div>{pizza.name}</div>
-                <div>{pizza.description}</div>
+    <div>
+      <div className="pizzaContainer">
+        <div style={{ flex: 2, margin: '10px' }}>
+          <List
+            dataSource={pizzas}
+            renderItem={(pizza: PizzaType) => (
+              <List.Item>
+                <div>
+                  <img
+                    width={50}
+                    height={50}
+                    src={pizza.url}
+                    onClick={() => handleSelectPizza(pizza)}
+                  />
+                </div>
+                <div className="pizzaDetails" onClick={() => handleSelectPizza(pizza)}>
+                  <div>{pizza.name}</div>
+                  <div className="pizzaDescription">{pizza.description}</div>
+                  <div>${pizza.price}</div>
+                </div>
+                <div className="QuantityButton">
+                  <div
+                    className="ButtonFilled"
+                    onClick={() => {
+                      handleAddToCart(pizza);
+                      handleSelectPizza(pizza);
+                    }}>
+                    +
+                  </div>
+                  {cartItems?.find((e) => e.id === pizza.id) && (
+                    <div className="Button">
+                      {cartItems.find((e) => e.id === pizza.id)?.quantity}
+                    </div>
+                  )}
+                  <div
+                    className="ButtonFilled"
+                    onClick={() => {
+                      handleRemoveFromCart(pizza);
+                      handleSelectPizza(pizza);
+                    }}>
+                    -
+                  </div>
+                </div>
+              </List.Item>
+            )}
+          />
+        </div>
+        <div style={{ flex: 2, margin: '10px' }}>
+          {selectedPizza && (
+            <Card title={selectedPizza.name} style={{ width: '100%' }}>
+              <div
+                style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                <div>
+                  <p>{selectedPizza.description}</p>
+                  <p>Price: ${selectedPizza.price}</p>
+                </div>
+                <div>
+                  <img src={selectedPizza.url} width={50} height={50} />
+                </div>
               </div>
               <div className="QuantityButton">
                 <div
                   className="ButtonFilled"
                   onClick={() => {
-                    handleAddToCart(pizza);
+                    handleAddToCart(selectedPizza);
                   }}>
                   +
                 </div>
-                {cartItems?.find((e) => e.id === pizza.id) && (
-                  <div className="Button">{cartItems.find((e) => e.id === pizza.id)?.quantity}</div>
+                {cartItems?.find((e) => e.id === selectedPizza.id) && (
+                  <div className="Button">
+                    {cartItems.find((e) => e.id === selectedPizza.id)?.quantity}
+                  </div>
                 )}
                 <div
                   className="ButtonFilled"
                   onClick={() => {
-                    handleRemoveFromCart(pizza);
+                    handleRemoveFromCart(selectedPizza);
                   }}>
                   -
                 </div>
               </div>
-            </List.Item>
+            </Card>
           )}
-        />
-      </div>
-      <div style={{ flex: 1, margin: '10px' }}>
-        {selectedPizza && (
-          <Card title={selectedPizza.name} style={{ width: '100%' }}>
-            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-              <div>
-                <p>{selectedPizza.description}</p>
-                <p>Price: ${selectedPizza.price}</p>
-              </div>
-              <div>
-                <img src={selectedPizza.url} width={50} height={50} />
-              </div>
-            </div>
-            <div className="QuantityButton">
-              <div
-                className="ButtonFilled"
-                onClick={() => {
-                  handleAddToCart(selectedPizza);
-                }}>
-                +
-              </div>
-              {cartItems?.find((e) => e.id === selectedPizza.id) && (
-                <div className="Button">
-                  {cartItems.find((e) => e.id === selectedPizza.id)?.quantity}
-                </div>
-              )}
-              <div
-                className="ButtonFilled"
-                onClick={() => {
-                  handleRemoveFromCart(selectedPizza);
-                }}>
-                -
-              </div>
-            </div>
+        </div>
+        <div style={{ flex: 1, margin: '10px' }}>
+          <Card title={`Quantity: ${orderCount}`} style={{ width: 200 }}>
+            <p>Price: ${price.toFixed(2)}</p>
+            <AppButton type="primary">Checkout</AppButton>
           </Card>
-        )}
+        </div>
       </div>
-      {totalPrice}
     </div>
   );
 };
